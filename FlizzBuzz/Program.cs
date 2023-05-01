e32dd308569f2affc377c1cd220845f27e4b7061
Алексей Василенко <Alekseiy80@gmail.com>
2023-05-01 16:22:10 +0300

    Game FizzBuzz

---
+++ FlizzBuzz/Program.cs
@@ -0,0 +1,26 @@
+﻿//----------------------------
+// Василенко Алексей Юрьевич
+// игра FizzBuzz
+//----------------------------
+void FizzBuzz(int x)
+{
+   for (int i = 1; i <= x; i++)
+   {
+      if (i % 3 == 0)
+      {
+         Console.WriteLine("Fizz");
+      }
+      if (i % 5 == 0)
+      {
+         Console.WriteLine("Buzz");
+      }
+      if (i % 3 == 0 || i % 5 == 0)
+      {
+         Console.WriteLine("FizzBuzz");
+      }
+   }
+}
+Console.WriteLine("Игра FizzBuzz");
+Console.Write("Введите число -> ");
+int num = Convert.ToInt32(Console.ReadLine());
+FizzBuzz(num);
