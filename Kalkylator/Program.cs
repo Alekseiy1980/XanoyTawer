95db661ae0de669f0a1da9d892ee975dc856c413
Алексей Василенко <Alekseiy80@gmail.com>
2023-05-02 22:46:16 +0300

    Калькулятор v1.0

---
+++ Kalkylator/Program.cs
@@ -0,0 +1,40 @@
+﻿
+Console.WriteLine("Калькулятор v 1.0");
+double number;
+double result;
+double number2;
+char znak;
+bool flag = true;
+
+number = Convert.ToDouble(Console.Read());
+
+while (flag)
+{
+
+   znak = Convert.ToChar(Console.Read());
+   if (znak == '+')
+   {
+      number2 = Convert.ToDouble(Console.Read());
+      number += number2;
+   }
+   else if (znak == '-')
+   {
+      number2 = Convert.ToDouble(Console.Read());
+      number -= number2;
+   }
+   else if (znak == '*')
+   {
+      number2 = Convert.ToDouble(Console.Read());
+      number *= number2;
+   }
+   else if (znak == '/')
+   {
+      number2 = Convert.ToDouble(Console.Read());
+      number /= number2;
+   }
+   else if (znak == '=')
+   {
+      flag = false;
+   }
+}
+Console.WriteLine(number);
